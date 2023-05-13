6c1b18dc1c09c4cfec28d16bf4def8504d4417ea
meharpalbasi <100152988+meharpalbasi@users.noreply.github.com>
2023-05-13 17:06:51 +0100

    Update WineApp.py

--- WineApp.py
+++ WineApp.py
@@ -55,7 +55,7 @@ Y_test_preds = rf_classif.predict(X_test)
 # In[15]:
 
 
-st.title("Wine Type :red[Prediction] :bar_chart: :chart_with_upwards_trend: :🍷:")
+st.title("Wine Type :red[Prediction] :bar_chart: :chart_with_upwards_trend: 🍷")
 st.markdown("Predict Wine Type using Ingredients Values")
 
 tab1, tab2, tab3 = st.tabs(["Data :clipboard:", "Global Performance :weight_lifter:", "Local Performance :bicyclist:"])
